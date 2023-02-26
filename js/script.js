772a09002641cbf1ae31189e4445c9be32b66bac
Grooov3 <goiaba_caio@hotmail.com>
2023-02-26 18:48:59 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -93,8 +93,8 @@ function randomizer(min, max) {
 
 
 const ul = document.querySelector(".ul-target"),
-var input = document.querySelector(".input-target"),
-var tagNumb = document.querySelector(".details span");
+ input = document.querySelector(".input-target"),
+ tagNumb = document.querySelector(".details span");
 var valores = ["30,85", "37,20", "38,50", "42,00", "32,75", "35,00"];
 countValores();
 createValor();
